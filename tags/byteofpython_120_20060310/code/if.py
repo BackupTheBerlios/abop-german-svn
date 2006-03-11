f324017a955615e102238b870a7c90e14dfbf5a0
cito <cito@d803e975-4df0-0310-8042-dbc507ec814a>
2006-03-11 02:25:00 +0000

    "A Byte of Python" Version 1.20 auf Deutsch Release vom 10.03.2006

---
+++ tags/byteofpython_120_20060310/code/if.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+zahl = 23
+geraten = int(raw_input('Geben Sie eine ganze Zahl ein : '))
+
+if geraten == zahl:
+	print 'Glueckwunsch, Sie haben es erraten.' # hier faengt ein neuer Block an
+	print "(Aber Sie gewinnen leider keinen Preis!)" # hier hoert der neue Block auf
+elif geraten < zahl:
+	print 'Nein, die Zahl ist etwas hoeher.' # noch ein Block
+	# Sie koennen in dem Block tun, was sie wollen ...
+else:
+	print 'Nein, die Zahl ist etwas niedriger.'
+	# hierhin gelangt man, wenn geraten > zahl ist
+
+print 'Fertig.'
+# Diese letzte Anweisung wird immer am Ende nach der if-Anweisung ausgefuehrt
